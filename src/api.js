e2c9a03f0fd703c7cb6135f4d080cb55bdb375a2
rafael moraes <rafaelsm003@gmail.com>
2022-08-25 16:02:26 -0300

    feat: aplicação recebe o endpoint POST /login

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const loginRoute = require('./routes/login');
 
 // ...
 
@@ -7,6 +8,14 @@ const app = express();
 app.use(express.json());
 
 // ...
+
+app.use('/login', loginRoute);
+
+app.use((err, _req, res, _next) => {
+    const { message } = err;
+    console.log('//////////////////////////', message, '////////////////////');
+    return res.status(500).json({ message: 'Server error.' });
+});
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
