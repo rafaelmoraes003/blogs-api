60e0316b0f91701b272b0b510ce17bbcc7707855
rafael moraes <rafaelsm003@gmail.com>
2022-08-25 17:05:58 -0300

    feat: cria middleware de autenticação de jwt

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const auth = (req, res, next) => {
+    const { authorization: token } = req.headers;
+    try {
+        if (!token) {
+            return res.status(401).json({ message: 'Token not found' });
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET);
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+};
+
+module.exports = { auth };
