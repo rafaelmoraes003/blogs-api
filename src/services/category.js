ba336c2b1876fc23c73dbfd6b93c11b3894c250f
rafael moraes <rafaelsm003@gmail.com>
2022-08-25 19:03:39 -0300

    feat: cria service para criação de categoria

---
+++ src/services/category.js
@@ -0,0 +1,18 @@
+const { Category } = require('../database/models');
+const { categorySchema } = require('../schemas/category');
+
+const createCategory = async (name) => {
+    const { error } = categorySchema.validate({ name });
+
+    if (error) {
+        return { error: { code: 400, message: error.details[0].message } };   
+    }
+
+    const { id } = await Category.create({ name });
+
+    return { code: 201, data: { id, name } };
+};
+
+module.exports = {
+    createCategory,
+};
