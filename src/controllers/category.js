bed014913813f4c530baf8f253670a219d1c4586
rafael moraes <rafaelsm003@gmail.com>
2022-08-25 19:03:27 -0300

    feat: cria controler para criação de categoria

---
+++ src/controllers/category.js
@@ -0,0 +1,20 @@
+const categoryService = require('../services/category');
+
+const createCategory = async (req, res, next) => {
+    const { name } = req.body;
+    try {
+        const { error, code, data } = await categoryService.createCategory(name);
+
+        if (error) {
+            return res.status(error.code).json({ message: error.message });
+        }
+
+        return res.status(code).json(data);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    createCategory,
+};
