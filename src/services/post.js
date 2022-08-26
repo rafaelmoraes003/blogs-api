d02e1897724a2b82363c6677e5c9de91e7dac646
rafael moraes <rafaelsm003@gmail.com>
2022-08-26 01:58:18 -0300

    feat: cria service capaz de trazer o blog post baseado no id do banco de dados se ele existir

--- src/services/post.js
+++ src/services/post.js
@@ -18,6 +18,28 @@ const getAllPosts = async () => {
     return { code: 200, data: posts };
 };
 
+const getPostById = async (id) => {
+    const post = await BlogPost.findOne({
+        where: { id },
+        include: [
+            {
+                model: User,
+                as: 'user',
+                attributes: { exclude: ['password'] },
+            },
+            {
+                model: Category,
+                as: 'categories',
+                through: { attributes: [] },
+            },
+        ],
+    });
+
+    if (!post) return { error: { code: 404, message: 'Post does not exist' } };
+    return { code: 200, data: post };
+};
+
 module.exports = {
     getAllPosts,
+    getPostById,
 };
