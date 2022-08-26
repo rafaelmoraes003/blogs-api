d3d75f91dd434a5dce0f90d69afe568c8e3868c9
rafael moraes <rafaelsm003@gmail.com>
2022-08-26 01:58:05 -0300

    feat: cria controller capaz de trazer o blog post baseado no id do banco de dados se ele existir

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -9,6 +9,22 @@ const getAllPosts = async (_req, res, next) => {
     }
 };
 
+const getPostById = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const { error, code, data } = await postService.getPostById(id);
+
+        if (error) {
+            return res.status(error.code).json({ message: error.message });
+        }
+
+        return res.status(code).json(data);
+    } catch (error) {
+        next(error);
+    }
+};
+
 module.exports = {
     getAllPosts,
+    getPostById,
 };
