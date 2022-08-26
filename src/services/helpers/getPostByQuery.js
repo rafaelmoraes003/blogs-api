18b78c2d96a821d29d689bc64c212618f9a5f9c4
rafael moraes <rafaelsm003@gmail.com>
2022-08-26 15:45:23 -0300

    feat: cria helper capaz de retornar um array de blogs post que contenham em seu título ou conteúdo o termo passado na URL

---
+++ src/services/helpers/getPostByQuery.js
@@ -0,0 +1,20 @@
+const { Op } = require('sequelize');
+const { BlogPost, User, Category } = require('../../database/models');
+
+const getPostByQuery = async (term) => {
+    const posts = await BlogPost.findAll({
+        where: {
+            [Op.or]: [
+                { title: { [Op.substring]: term } },
+                { content: { [Op.substring]: term } }],
+        },
+        include: [
+            { model: User, as: 'user', attributes: { exclude: ['password'] } },
+            { model: Category, as: 'categories', through: { attributes: [] } },
+        ],
+    });
+
+    return posts;
+};
+
+module.exports = { getPostByQuery };
